0b43b64bcfad3d4390093892709b0857b8526a55
Deepkumar Patel <dspguru97@gmail.com>
2021-04-12 18:43:25 -0400

    hero-content-is-up

--- src/components/herocontent.js
+++ src/components/herocontent.js
@@ -2,7 +2,27 @@ import React from "react";
 
 function hero(){
     return(
-        <h1>Hello World</h1>
+        <section className="hero-section">
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="com-tagline">
+                        <h2>
+                            experts<br/> 
+                            in digital<br/>
+                            know-how
+                        </h2>
+                    </div>
+                    <div className="subtext">
+                        Built by <a href="#">Deep Patel</a>, and referenced by <a href="#">Dribble</a>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="hero-image">
+                        <img src="./images/hero.png" alt="Hero Illustration"/>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 export default hero;
