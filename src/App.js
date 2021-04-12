e2b817cbe459be11a478e5bd1ec4d6022ed439cf
Deepkumar Patel <dspguru97@gmail.com>
2021-04-12 17:49:19 -0400

    Navbar-almost-done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.scss';
+// import components
+import Navbar from './components/navbar';
+// bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App bg-dark">
-      <h1>React App</h1>
+    <div className="App">
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function navbar(){
+    return(
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <a className="navbar-brand" href="#">hub</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav nav-first-block">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Find jobs</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Browse startups</a>
+                        </li>
+                    </ul>
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">For startups</a>
+                        </li>
+                        <li>
+                            <div className="hor-line"></div>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Sign in</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default navbar;
