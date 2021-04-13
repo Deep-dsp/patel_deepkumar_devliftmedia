ca098b31e55805de928deccb21e4496d8b994f49
Deepkumar Patel <dspguru97@gmail.com>
2021-04-13 09:41:06 -0400

    client-section-done

--- src/components/clientThumb.js
+++ src/components/clientThumb.js
@@ -3,30 +3,30 @@ import React from "react";
 function clientthumb(){
     return(
         <section className="client-section">
-            <div className="row">
+            <div className="row d-flex align-items-center">
                 
-                <div className="col-lg-4 d-flex justify-content-center align-items-center">
+                <div className="client-text col-sm-12 col-lg-3 d-flex justify-content-center align-items-center">
                     <p>Companies We've Helped Build</p>
                 </div>
-                <div className="col-lg-8">
-                    <div className="row">
-                        <div className="col-lg-2 d-flex align-items-center">
-                            Hello
+                <div className="client-thumbnail col-sm-12 col-lg-9">
+                    <div className="row thumb-row">
+                        <div className="col-sm-12 col-md-2 d-flex justify-content-center align-items-center">
+                            <img src="./images/1.png" alt="esa logo"/>
                         </div>
-                        <div className="col-lg-2 d-flex align-items-center">
-                            Hello
+                        <div className="col-sm-12 col-md-2 d-flex justify-content-center align-items-center">
+                            <img src="./images/2.png" alt="vmcare logo"/>
                         </div>
-                        <div className="col-lg-2 d-flex align-items-center">
-                            Hello
+                        <div className="col-sm-12 col-md-2 d-flex justify-content-center align-items-center">
+                            <img src="./images/3.png" alt="google logo"/>
                         </div>
-                        <div className="col-lg-2 d-flex align-items-center">
-                            Hello
+                        <div className="col-sm-12 col-md-2 d-flex justify-content-center align-items-center">
+                            <img src="./images/4.png" alt="amazon logo"/>
                         </div>
-                        <div className="col-lg-2 d-flex align-items-center">
-                            Hello
+                        <div className="col-sm-12 col-md-2 d-flex justify-content-center align-items-center">
+                            <img src="./images/5.png" alt="graphisoft logo"/>
                         </div>
-                        <div className="col-lg-2 d-flex align-items-center">
-                            Hello
+                        <div className="col-sm-12 col-md-2 d-flex justify-content-center align-items-center">
+                            <img src="./images/6.png" alt="Irex logo"/>
                         </div>
                     </div>
                 </div>
