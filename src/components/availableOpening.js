0f31defe1957dc2fef24993d1e0e3ce45e76656e
Deepkumar Patel <dspguru97@gmail.com>
2021-04-13 22:30:15 -0400

    job-section-done

--- src/components/availableOpening.js
+++ src/components/availableOpening.js
@@ -11,7 +11,7 @@ function jobopening(){
                     <p>Find a job you love. <a href="#">Set career interests.</a></p>
                 </div>
                 <div className="explore-category row">
-
+                    {/* First block */}
                     <div className="col-md-4">
                         <div className="box">
                             <div className="box-body">
@@ -41,7 +41,162 @@ function jobopening(){
                             </div>
                         </div>
                     </div>
+                    {/* block */}
+                    <div className="col-md-4">
+                        <div className="box">
+                            <div className="box-body">
+                                <div className="box-first-block">
+                                    
+                                    <div className="title-circle d-flex align-items-center"> 
+                                        <div className="circle"></div>
+                                        <p>development</p>
+                                    </div>
+                                    <div className="jobpost-title"> Senior B2B Sales <br/>Consultant - Sweden</div>
+                                    <div className="jobpost-location d-flex align-items-center">
+                                        <p>London, ON</p>
+                                        <p>&#9679;</p>
+                                        <p>Full-time</p>
+                                    </div>
 
+                                </div>
+                                <div className="box-second-block d-flex align-items-center">
+                                    <div className="job-thumb">
+                                        <img src="https://dummyimage.com/50"/>
+                                    </div>
+                                    <div className="company-name d-flex align-items-center">
+                                        <p>founders,</p>
+                                        <p>3 days ago</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* block */}
+                    <div className="col-md-4">
+                        <div className="box">
+                            <div className="box-body">
+                                <div className="box-first-block">
+                                    
+                                    <div className="title-circle d-flex align-items-center"> 
+                                        <div className="circle"></div>
+                                        <p>Marketing</p>
+                                    </div>
+                                    <div className="jobpost-title"> Marketing & Communications Manager(Internship/Co-founder)</div>
+                                    <div className="jobpost-location d-flex align-items-center">
+                                        <p>London, ON</p>
+                                        <p>&#9679;</p>
+                                        <p>part-time</p>
+                                    </div>
+
+                                </div>
+                                <div className="box-second-block d-flex align-items-center">
+                                    <div className="job-thumb">
+                                        <img src="https://dummyimage.com/50"/>
+                                    </div>
+                                    <div className="company-name d-flex align-items-center">
+                                        <p>founders,</p>
+                                        <p>3 days ago</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* First block */}
+                    <div className="col-md-4">
+                        <div className="box">
+                            <div className="box-body">
+                                <div className="box-first-block">
+                                    
+                                    <div className="title-circle d-flex align-items-center"> 
+                                        <div className="circle"></div>
+                                        <p>design</p>
+                                    </div>
+                                    <div className="jobpost-title"> Marketing & Communications Manager(Internship/Co-founder)</div>
+                                    <div className="jobpost-location d-flex align-items-center">
+                                        <p>London, ON</p>
+                                        <p>&#9679;</p>
+                                        <p>Full-time</p>
+                                    </div>
+
+                                </div>
+                                <div className="box-second-block d-flex align-items-center">
+                                    <div className="job-thumb">
+                                        <img src="https://dummyimage.com/50"/>
+                                    </div>
+                                    <div className="company-name d-flex align-items-center">
+                                        <p>founders,</p>
+                                        <p>3 days ago</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* block */}
+                    <div className="col-md-4">
+                        <div className="box">
+                            <div className="box-body">
+                                <div className="box-first-block">
+                                    
+                                    <div className="title-circle d-flex align-items-center"> 
+                                        <div className="circle"></div>
+                                        <p>development</p>
+                                    </div>
+                                    <div className="jobpost-title"> Senior B2B Sales <br/>Consultant - Sweden</div>
+                                    <div className="jobpost-location d-flex align-items-center">
+                                        <p>London, ON</p>
+                                        <p>&#9679;</p>
+                                        <p>Full-time</p>
+                                    </div>
+
+                                </div>
+                                <div className="box-second-block d-flex align-items-center">
+                                    <div className="job-thumb">
+                                        <img src="https://dummyimage.com/50"/>
+                                    </div>
+                                    <div className="company-name d-flex align-items-center">
+                                        <p>founders,</p>
+                                        <p>3 days ago</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* block */}
+                    <div className="col-md-4">
+                        <div className="box">
+                            <div className="box-body">
+                                <div className="box-first-block">
+                                    
+                                    <div className="title-circle d-flex align-items-center"> 
+                                        <div className="circle"></div>
+                                        <p>Marketing</p>
+                                    </div>
+                                    <div className="jobpost-title"> Marketing & Communications Manager(Internship/Co-founder)</div>
+                                    <div className="jobpost-location d-flex align-items-center">
+                                        <p>London, ON</p>
+                                        <p>&#9679;</p>
+                                        <p>part-time</p>
+                                    </div>
+
+                                </div>
+                                <div className="box-second-block d-flex align-items-center">
+                                    <div className="job-thumb">
+                                        <img src="https://dummyimage.com/50"/>
+                                    </div>
+                                    <div className="company-name d-flex align-items-center">
+                                        <p>founders,</p>
+                                        <p>3 days ago</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="job-button d-flex justify-content-center">
+                    <button className="btn">Show all jobs</button>
                 </div>
             </div>
         </section>
